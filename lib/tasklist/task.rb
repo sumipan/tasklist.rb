6027c73b41d2924b1b7fe8307986a76c37aea7b8
takashi nagayasu <regist@g-onion.org>
2014-12-12 05:40:03 +0900

    Fix

--- lib/tasklist/task.rb
+++ lib/tasklist/task.rb
@@ -4,19 +4,20 @@ module Tasklist
   class Task
     include Contracts
 
-    attr_accessor :title,  :assignee, :remaining_time, :is_done
+    attr_reader :title,  :assignee, :remaining_time, :is_done
 
     Contract ({ :title => String, :assignee => String, :remaining_time => Fixnum, :is_done => Bool }) => nil
     def initialize(params)
       [:title, :assignee, :remaining_time, :is_done].each do |key|
         if params.include?(key) then
-          send("#{key.to_s}=", params[:key])
+          instance_eval "@#{key.to_s}=params[key]"
         end
       end
 
       nil
     end
 
+    Contract () => Bool
     def is_done?
       is_done
     end
--- test/test_task.rb
+++ test/test_task.rb
@@ -7,12 +7,17 @@ describe Tasklist::Task do
   it "argument is checked type" do
     task = Tasklist::Task.new({
       :title          => 'task title',
-      :assignee       => 'assignee name',
+      :assignee       => 'sumipan',
       :remaining_time => 2,
       :is_done        => false,
     })
 
     task.must_be_instance_of Tasklist::Task
+
+    task.title.must_equal 'task title'
+    task.assignee.must_equal 'sumipan'
+    task.remaining_time.must_equal 2
+    task.is_done?.must_equal false
 
     proc {
       Tasklist::Task.new({
